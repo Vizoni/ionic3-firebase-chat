9b1d8475b60e3aff96315d784cf9953e12eaf501
Raphael Vizoni <ravizoni@gmail.com>
2017-11-24 16:47:37 -0200

    Configurando Firebase no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2'; //importa o firebase app config
+
+/* Salva as configurações do firebase (pega no painel do projeto no site do firebase) em uma constante */
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyAltwD95zzGwltXs8QS98rU1izkuzwpS0Q",
+  authDomain: "ionic2-firebase-chat-c582c.firebaseapp.com",
+  databaseURL: "https://ionic2-firebase-chat-c582c.firebaseio.com",
+  storageBucket: "ionic2-firebase-chat-c582c.appspot.com",
+  messagingSenderId: "717491479259"
+};
 
 @NgModule({
   declarations: [
@@ -13,6 +23,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseAppConfig),
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
