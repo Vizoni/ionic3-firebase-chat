b276a8a19926ccda68abb6c42beb45f5c12eb52f
Raphael Vizoni <ravizoni@gmail.com>
2017-11-27 23:15:30 -0200

    aula 70 - listando usuarios cadastrados

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { FirebaseListObservable } from 'angularfire2/database';
+
 import { SignupPage } from '../signup/signup';
+import { User } from '../../models/user.model';
+import { UserService } from './../../providers/user/user.service';
 
 @Component({
   selector: 'page-home',
@@ -9,13 +13,28 @@ import { SignupPage } from '../signup/signup';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  users: FirebaseListObservable<User[]>;  // atributo users é uma array de Users do tipo Observable do FireBase
 
+  constructor(
+    public navCtrl: NavController,
+    public userService: UserService,  // injeta o user service
+  ) {
+
+  }
+
+  ionViewDidLoad() {
+    this.users = this.userService.users;  // o atributo users dessa página é o mesmo q o atributo do user service   
+    console.log("usuarios cadastrados LISTADOS!", this.users);
   }
 
   onSignup(): void {
-    console.log("teste");
     this.navCtrl.push(SignupPage);
   }
+
+  onChatCreate(user: User): void {
+    console.log('user:',user);
+  }
+
+  
 
 }
--- src/providers/user/user.service.ts
+++ src/providers/user/user.service.ts
@@ -2,21 +2,28 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-import { AngularFire } from 'angularfire2';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { User } from '../../models/user.model'; // importa a classe de User que compõe o formulário
 
 @Injectable()
 export class UserService {
 
+  users: FirebaseListObservable<User[]>;  // atributo array de usuarios do tipo (model) User
+
   constructor(
     public af: AngularFire, // injeta o angular fire pra poder mexer com o real time
     public http: Http,
-  ) {}
+  ) {
+    this.users = this.af.database.list(`/users`);
+  }
 
   create(user: User): firebase.Promise<void> { 
     // A função create tem o parametro user do tipo User (pasta models) e retorna uma firebase.promise VAZIA (void)
+    /*
     return this.af.database.list(`/users`)  // lista os usuarios (nó 'users')
       .push(user);  // adiciona o parametro passado (objeto do formulario) na lista dos nós '/users'
+      */
+    return this.users.push(user); // o atributo users é uma listagem do nó '/users'. O método push é pra adicionar
   }
 
 
