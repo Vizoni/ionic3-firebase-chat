3805497e07b703c9317d5a983e588eab7b90fbc5
Raphael Vizoni <ravizoni@gmail.com>
2017-11-28 12:23:54 -0200

    aula 79 - sigIn com email

--- src/providers/auth/auth.service.ts
+++ src/providers/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { AngularFireAuth, FirebaseAuthState } from 'angularfire2/auth';
+import { AngularFireAuth, FirebaseAuthState, firebaseAuthConfig } from 'angularfire2/auth';
 import { BaseService } from '../base/base.service';
+import { FirebaseError } from 'firebase';
 
 @Injectable()
 export class AuthService extends BaseService{
@@ -19,6 +20,14 @@ export class AuthService extends BaseService{
     // cria um usuario de autenticação com o serviço Angular Fire Auth
     return this.auth.createUser(user)
       .catch(this.handlePromiseError);  //o erro do catch passa pra função handlePromiseError do BaseService
+  }
+
+  signInWithEmail(user: {email: string, password: string}): firebase.Promise<boolean> {
+    return this.auth.login(user)
+      .then((authState: FirebaseAuthState) => {
+        return authState != null; // retorna true se o authState for diferente de nulo (ou seja, logou)
+      })
+      .catch(this.handlePromiseError);
   }
 
 }
