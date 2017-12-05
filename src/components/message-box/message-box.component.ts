550cafe67a52e66d44caa4869e030df2dd6c68dd
Raphael Vizoni <ravizoni@gmail.com>
2017-12-05 13:59:41 -0200

    aula 113 - ajustes scss

--- src/components/message-box/message-box.component.ts
+++ src/components/message-box/message-box.component.ts
@@ -2,9 +2,19 @@ import { Component, Input } from '@angular/core';
 
 import { Message } from './../../models/message.model';
 
+/* no host fica tipo assim
+  <message-box [style.justify-content]="flex-start"> </message-box>
+*/
+
 @Component({
   selector: 'message-box',
-  templateUrl: 'message-box.component.html'
+  templateUrl: 'message-box.component.html',
+  host: {   
+    // pode pegar propriedades no elemento e fazer input property (colocando um estilo (classe) condicionalmente)
+    '[style.justify-content]': '((!isFromSender) ? "flex-start" : "flex-end")',
+    // Se NÃO for do remetente, fica na esquerda, se for, fica na direita 
+    // '[style.text-align]': '((!isFromSender) ? "left" : "right")'
+  }
 })
 export class MessageBoxComponent {
 
