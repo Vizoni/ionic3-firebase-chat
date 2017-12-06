ae912644fc9ce2470a1770a9c5e476313a7d2e97
Raphael Vizoni <ravizoni@gmail.com>
2017-12-06 21:15:02 -0200

    aula 136 - exibindo foto do usuario no chatPage

--- src/components/custom-logged-header/custom-logged-header.component.ts
+++ src/components/custom-logged-header/custom-logged-header.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { baseComponent } from '../base.component';
 import { AlertController, App, MenuController  } from 'ionic-angular';
 import { AuthService } from '../../providers/auth/auth.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'custom-logged-header',
@@ -10,6 +11,7 @@ import { AuthService } from '../../providers/auth/auth.service';
 export class CustomLoggedHeaderComponent extends baseComponent{
 
   @Input() title: string;
+  @Input() user: User;
   // @input é uma propriedade visivel para os outros components
 
   constructor(
