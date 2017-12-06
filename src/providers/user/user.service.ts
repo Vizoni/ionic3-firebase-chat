cae5ea1f2d19ed0bccd91b7691549be45875cd4b
Raphael Vizoni <ravizoni@gmail.com>
2017-12-06 13:32:04 -0200

    aula 124 - método de editar usuário no userService

--- src/providers/user/user.service.ts
+++ src/providers/user/user.service.ts
@@ -56,6 +56,12 @@ export class UserService extends BaseService{
 
   }
 
+  edit(user: {name: string, username: string, photo: string}): firebase.Promise<void> {
+    return this.currentUser
+      .update(user)
+      .catch(this.handlePromiseError)
+  }
+
   userExists(username: string): Observable<boolean> {
     return this.af.database.list(`/users`, {
       query: {
